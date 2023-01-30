35bf33a85e0fd5bc5738a5401d43199c91bbed47
Ksawery Oleszynski <ksawery.oleszynski@gmail.com>
2023-01-30 20:33:45 +0100

    Initial commit

---
+++ StudentsApi/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentsApi.Controllers;
+
+[Route("api/students")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult GetStudent()
+    {
+        var list = new List<StudentDetails>();
+        return Ok(list);
+    }
+    
+}
+
+public class StudentDetails
+{
+}
